f8f1df00ed6e5ab2e00b9415f08077407b1771a2
arfeen <arfeen14@hotmail.com>
2020-05-06 01:24:25 +0200

    Api gemaakt shopify

--- app/src/main/java/shopify/api/ShopifyApi.kt
+++ app/src/main/java/shopify/api/ShopifyApi.kt
@@ -1,4 +1,31 @@
 package shopify.api
 
+
+import com.example.arfeenshopapp.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 class ShopifyApi {
+    companion object {
+        //base url off the api
+
+        private var baseUrl =
+            "https://" + BuildConfig.SHOPIFY_KEY + ":" + BuildConfig.SHOPIFY_PWD + "@arfeensonlineshop.myshopify.com/admin/api/"
+
+        fun createApi(): ShopifyApiService {
+            val okHttpClient = OkHttpClient.Builder()
+                .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+                .build()
+
+            val shopifyApi = Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+            return shopifyApi.create(ShopifyApiService::class.java)
+        }
+    }
 }
