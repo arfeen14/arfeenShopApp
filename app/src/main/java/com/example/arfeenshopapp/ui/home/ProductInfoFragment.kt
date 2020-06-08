0a3d84cac2f3eb3604230cc22b54b4d56d443c97
arfeen <arfeen14@hotmail.com>
2020-06-08 12:05:58 +0200

    Delete function addes with swipe functionality(rubric instead of button)

--- app/src/main/java/com/example/arfeenshopapp/ui/home/ProductInfoFragment.kt
+++ app/src/main/java/com/example/arfeenshopapp/ui/home/ProductInfoFragment.kt
@@ -1,30 +1,19 @@
 package com.example.arfeenshopapp.ui.home
 
-import android.content.Intent
 import android.os.Bundle
 import android.text.Html
-import android.util.Log
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
-import androidx.navigation.fragment.findNavController
-import com.bumptech.glide.Glide
-import com.example.arfeenshopapp.MainActivity
 import com.example.arfeenshopapp.R
-import com.example.arfeenshopapp.adapter.CategoryAdapter
-import com.example.arfeenshopapp.ui.CategoryFragment
-import com.example.arfeenshopapp.ui.notifications.NotificationsFragment
-import com.example.arfeenshopapp.ui.notifications.NotificationsViewModel
 import data.ProductenRepository
-import kotlinx.android.synthetic.main.product_info.*
 import kotlinx.android.synthetic.main.product_info.view.*
-import kotlinx.android.synthetic.main.product_item.view.*
-import kotlinx.android.synthetic.main.wishlist_item.*
-import kotlinx.android.synthetic.main.wishlist_item.view.*
-import models.Product
 import models.Producten
+
 
 class ProductInfoFragment : Fragment() {
 
@@ -64,14 +53,12 @@ class ProductInfoFragment : Fragment() {
 //            .into(root.imgProduct_info)
 
 
-
         root.tvProduct_info_name.text = "€ " + geselecteerdeProduct!!.variants[0].productPrice
 
         productenRepository = ProductenRepository(requireContext())
 
 
         root.btnAddToWishList.setOnClickListener() {
-            //hier moet ik proberen een product toe te voegen
             addProduct()
         }
 
@@ -80,6 +67,7 @@ class ProductInfoFragment : Fragment() {
     }
 
     fun addProduct() {
+     
         productenRepository.insertProducten(geselecteerdeProduct!!)
     }
 
--- app/src/main/java/com/example/arfeenshopapp/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/arfeenshopapp/ui/notifications/NotificationsFragment.kt
@@ -8,15 +8,21 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.arfeenshopapp.R
 import com.example.arfeenshopapp.adapter.WenslijstAdapter
 import data.ProductenRepository
+import kotlinx.android.synthetic.main.fragment_notifications.*
 
 import kotlinx.android.synthetic.main.product_info.view.*
 import kotlinx.android.synthetic.main.wishlist_item.view.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import models.Producten
 
 class NotificationsFragment : Fragment() {
@@ -43,11 +49,16 @@ class NotificationsFragment : Fragment() {
 
         val root = inflater.inflate(R.layout.fragment_notifications, container, false)
 
+
+
         val rvWenslijst: RecyclerView = root.findViewById(R.id.rvWenslijst1)
 
         rvWenslijst.layoutManager =
             LinearLayoutManager(this.context, LinearLayoutManager.VERTICAL, false)
         rvWenslijst.adapter = wenslijstAdapter
+
+        createItemTouchHelper().attachToRecyclerView(rvWenslijst)
+
 
         productenRepository = ProductenRepository(requireContext())
         getProductenFromDatabase()
@@ -61,6 +72,37 @@ class NotificationsFragment : Fragment() {
         this.product.clear()
         this.product.addAll(producten)
         wenslijstAdapter.notifyDataSetChanged()
+    }
+
+    private fun createItemTouchHelper(): ItemTouchHelper {
+        val callBack = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+
+            // Enables or Disables the ability to move items up and down.
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            // Callback triggered when a user swiped an item.
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+
+                val productToDelete = product[position]
+
+                CoroutineScope(Dispatchers.Main).launch {
+                    withContext(Dispatchers.IO) {
+                        productenRepository.deleteProducten(productToDelete)
+                    }
+                    getProductenFromDatabase()
+                }
+
+
+            }
+        }
+        return ItemTouchHelper(callBack)
     }
 
 }
